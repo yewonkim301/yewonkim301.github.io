ff65b2a39ba4e71faecceca27f6d00ec6a355eda
yewonkim301 <yeowne301@gmail.com>
2024-06-19 16:26:58 +0900

    build

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1718779007',resources: [
+    cacheName: 'chirpy-1718781988',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
