7f03bfb7287b705dba180f893c5019cfb17ae39d
yewonkim301 <yeowne301@gmail.com>
2024-06-21 15:57:26 +0900

    update avatar img

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1718872585',resources: [
+    cacheName: 'chirpy-1718952372',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
