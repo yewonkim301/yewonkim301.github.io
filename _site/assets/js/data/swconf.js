03731f586dba42283253264cb9b51ee798a7b60a
yewonkim301 <yeowne301@gmail.com>
2024-10-28 18:56:16 +0900

    post : 241028 TIL

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1718952372',resources: [
+    cacheName: 'chirpy-1718954579',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
